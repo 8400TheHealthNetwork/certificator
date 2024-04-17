67eae884d4c3365693b91ca5d059c514d6e7afa1
Daniel Mechanik <daniel@outburn.co.il>
2024-04-17 14:45:33 +0300

    added download-assets script

---
+++ src/fhirPackages.ts
@@ -0,0 +1,66 @@
+export const packages: Array<Record<string, string>> = [
+  {
+    packageName: 'hl7.fhir.r4.core',
+    version: '4.0.1'
+  },
+  {
+    packageName: 'hl7.fhir.r4.examples',
+    version: '4.0.1'
+  },
+  {
+    packageName: 'hl7.fhir.us.core',
+    version: '6.1.0'
+  },
+  {
+    packageName: 'hl7.fhir.uv.bulkdata',
+    version: '2.0.0'
+  },
+  {
+    packageName: 'hl7.fhir.uv.extensions',
+    version: '1.0.0'
+  },
+  {
+    packageName: 'hl7.fhir.uv.extensions.r4',
+    version: '1.0.0'
+  },
+  {
+    packageName: 'hl7.fhir.uv.sdc',
+    version: '3.0.0'
+  },
+  {
+    packageName: 'hl7.fhir.uv.smart-app-launch',
+    version: '2.1.0'
+  },
+  {
+    packageName: 'hl7.fhir.xver-extensions',
+    version: '0.1.0'
+  },
+  {
+    packageName: 'hl7.terminology',
+    version: '5.5.0'
+  },
+  {
+    packageName: 'hl7.terminology.r4',
+    version: '5.0.0'
+  },
+  {
+    packageName: 'hl7.terminology.r5',
+    version: '5.0.0'
+  },
+  {
+    packageName: 'ihe.formatcode.fhir',
+    version: '1.1.0'
+  },
+  {
+    packageName: 'il.core.fhir.r4',
+    version: '0.14.0'
+  },
+  {
+    packageName: 'us.cdc.phinvads',
+    version: '0.12.0'
+  },
+  {
+    packageName: 'us.nlm.vsac',
+    version: '0.11.0'
+  }
+];
