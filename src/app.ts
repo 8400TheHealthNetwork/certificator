66192ee037d5a4da02907d55c2af3ded6d919657
mechanik-daniel <daniel@outburn.co.il>
2024-09-17 16:04:16 +0300

    feat: register middleware and custom logger

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,27 @@ import { IConfig, FumeServer } from 'fume-fhir-converter';
 import { extraBindings } from './helpers';
 import { setFumeServer } from './config';
 import { loadMapFiles } from './helpers/loadMaps';
-import type { Request, Response } from 'express';
+import { version as CERTIFICATOR_VERSION } from '../package.json';
+import type { Request, Response, NextFunction } from 'express';
 
 let configObject: IConfig;
 
-const healthCheck = async (req: Request, res: Response) => {
-  res.status(200).json({
-    FHIR_SERVER_BASE: configObject.FHIR_SERVER_BASE,
-    CERTIFICATOR_API_PORT: configObject.SERVER_PORT
-  });
+const reRouter = async (req: Request, res: Response, next: NextFunction) => {
+  if (req.method.toLowerCase() === 'get' && (req.originalUrl === '/' || req.originalUrl === '/health')) {
+    res.status(200).json({
+      CERTIFICATOR_VERSION,
+      FHIR_SERVER_BASE: configObject.FHIR_SERVER_BASE,
+      CERTIFICATOR_API_PORT: configObject.SERVER_PORT
+    });
+  } else {
+    next();
+  }
+};
+
+const logger = {
+  info: () => { /* Do nothing with info level logging from FUME */ },
+  warn: (input: any | any[]) => console.warn(input),
+  error: (input: any | any[]) => console.error(input)
 };
 
 async function initServer () {
@@ -23,15 +35,17 @@ async function initServer () {
     checkValidator();
     checkMaps();
     const fumeServer = new FumeServer<IConfig>();
-    setFumeServer(fumeServer);
+    fumeServer.registerLogger(logger);
+    fumeServer.registerAppMiddleware(reRouter);
+    console.log('Registered HTTP server middleware');
     for (const key in extraBindings) {
       fumeServer.registerBinding(key, extraBindings[key]);
+      console.log(`Registered $${key}() function binding`);
     }
-    console.log('Registered extra bindings');
-    const app = fumeServer.getExpressApp();
-    app.get('/health', healthCheck);
+    setFumeServer(fumeServer);
     await fumeServer.warmUp(newConfig);
     loadMapFiles();
+    console.log('Ready :)');
   } catch (err) {
     console.error('Error warming up: ', err);
   }
