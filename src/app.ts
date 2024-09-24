44b56982029da0b16f87bee74c2937f17c9b80ac
mechanik-daniel <daniel@outburn.co.il>
2024-09-24 16:26:39 +0300

    feat: start implementing run status files (wip)

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,82 @@
 import config from './serverConfig';
 import fs from 'fs-extra';
-import { ensureEnv, checkPackages, checkValidator, checkMaps } from './setup';
+import { ensureEnv, checkPackages, checkValidator, checkMaps, ensureRunsDir } from './setup';
 import { fork } from 'node:child_process';
 import path from 'path';
 import cors from 'cors';
 import express from 'express';
 import axios from 'axios';
-import type { Express, Request, Response } from 'express';
 import chalk from 'chalk';
 import kits from '../kits.json';
 import jsonata from 'jsonata';
-import type { Expression } from 'jsonata';
 import contentTypeMap from './helpers/contentTypeMap.json';
+import type { Expression } from 'jsonata';
+import type { Express, Request, Response } from 'express';
 
 const app: Express = express();
 const port: number = 8400;
 const workingDir = path.resolve('.');
 const uiDistPath = path.join(workingDir, 'ui', 'dist');
-let getKitTransformer: Expression;
-let getKitstransformer: Expression;
+const currentRunDir = path.join(workingDir, 'runs', 'current');
+const ioDir = path.join(workingDir, 'io');
+const getKitTransformer: Expression = jsonata(`
+  (
+    $actionsMap := actions{
+      id: description
+    };
+
+    kits[id=$kitId].{
+      'children': children.{
+        'id': id,
+        'name': name,
+        'metadata': description ? {
+          'description': description,
+          'status': 'ready'
+        },
+        'children': children.{
+          'id': id,
+          'name': name,
+          'metadata': {
+            'status': 'ready'
+          },
+          'children': children.{
+            'id': id,
+            'name': name,
+            'metadata': {
+              'Description': description,
+              'Status': 'ready',
+              'Details': details,
+              'Actions': '\n' & (actions#$i.(
+                $string($i + 1) & '. ' & $lookup($actionsMap, $) & ' (ready)'
+              ) ~> $join( '\n'))
+            }
+          }[]
+        }[]
+      }[]
+    }
+  )
+`);
+const getKitsTransformer: Expression = jsonata(`
+  {
+    'kits': kits.{
+      'id': id,
+      'name': name,
+      'description': description,
+      'status': $getKitStatus(id)
+    }[]
+  }`
+);
+const runWorkflowTransformer: Expression = jsonata(`
+    {
+      'timestamp': $fromMillis($millis(), '[Y0000][M00][D00]_[H00][m00][s00]'),
+      'kitId': $kitId,
+      'tests': kits[id = $kitId].children.children.children.{
+        'testId': id,
+        'skipped': $not(id in $selectedTests)
+      }[]
+    }
+  `
+);
 
 const engineApi = axios.create({
   baseURL: 'http://localhost:8401'
@@ -37,82 +95,61 @@ const isFiletypeChunked = {
 };
 
 const getKitStatus = (kitId: string) => {
-  // TODO: implement
+  const kitStatusFilePath = path.join(currentRunDir, 'kitStatus.json');
+  if (fs.existsSync(kitStatusFilePath)) {
+    const kitStatusFileContent = JSON.parse(fs.readFileSync(kitStatusFilePath).toString());
+    if (kitStatusFileContent?.kitId === kitId) {
+      return kitStatusFileContent?.status;
+    };
+  };
   return 'ready';
 };
 
 const getKits = async (res: Response) => {
-  if (!getKitstransformer) {
-    getKitstransformer = jsonata(`
-      {
-        'kits': kits.{
-          'id': id,
-          'name': name,
-          'description': description,
-          'status': $getKitStatus(id)
-        }[]
-      }`
-    );
-  };
-  const transformed = await getKitstransformer.evaluate(kits, { getKitStatus });
+  const transformed = await getKitsTransformer.evaluate(kits, { getKitStatus });
   res.status(200).json(transformed);
 };
 
 const getKit = async (req: Request, res: Response) => {
   const kitId: string = req.originalUrl.substring(10);
-  if (!getKitTransformer) {
-    getKitTransformer = jsonata(`
-      (
-        $actionsMap := actions{
-          id: description
-        };
-  
-        kits[id=$kitId].{
-          'children': children.{
-            'id': id,
-            'name': name,
-            'metadata': description ? {
-              'description': description,
-              'status': 'ready'
-            },
-            'children': children.{
-              'id': id,
-              'name': name,
-              'metadata': {
-                'status': 'ready'
-              },
-              'children': children.{
-                'id': id,
-                'name': name,
-                'metadata': {
-                  'Description': description,
-                  'Status': 'ready',
-                  'Details': details,
-                  'Actions': '\n' & (actions#$i.(
-                    $string($i + 1) & '. ' & $lookup($actionsMap, $) & ' (ready)'
-                  ) ~> $join( '\n'))
-                }
-              }[]
-            }[]
-          }[]
-        }
-      )
-    `);
-  };
   const transformed = await getKitTransformer.evaluate(kits, { kitId });
   res.status(200).json(transformed);
 };
 
 const runKit = async (req: Request, res: Response) => {
   res.status(202).send('Accepted');
+  const kitId: string = req.body.kitId;
+  const selectedTests: string[] = req.body.selected;
+  const workflow = await runWorkflowTransformer.evaluate(kits, { kitId, selectedTests });
+  fs.ensureDirSync(currentRunDir);
+  fs.writeFileSync(path.join(currentRunDir, 'workflow.json'), JSON.stringify(workflow));
+  fs.writeFileSync(path.join(currentRunDir, 'kitStatus.json'), JSON.stringify({ kitId, status: 'in-progress' }));
 };
 
-const abortRun = async (req: Request, res: Response) => {
-  res.status(202).send('Accepted');
+const abortRun = (res?: Response) => {
+  if (res) res.status(202).send('Accepted');
+  const kitStatusFilePath: string = path.join(currentRunDir, 'kitStatus.json');
+  if (fs.existsSync(kitStatusFilePath)) {
+    const kitStatusFileContent = JSON.parse(fs.readFileSync(kitStatusFilePath).toString());
+    const kitId: string = kitStatusFileContent.kitId;
+    const currentStatus: string = kitStatusFileContent.status;
+    if (currentStatus === 'in-progress') fs.writeFileSync(path.join(currentRunDir, 'kitStatus.json'), JSON.stringify({ kitId, status: 'aborted' }));
+  }
 };
 
-const stashRun = async (req: Request, res: Response) => {
+const stashRun = (res: Response) => {
   // move current run dir into stash and clear it
+  const workflowFilePath = path.join(currentRunDir, 'workflow.json');
+  if (fs.existsSync(workflowFilePath)) {
+    const workflowFileContent = JSON.parse(fs.readFileSync(workflowFilePath).toString());
+    const runTimestamp = workflowFileContent?.timestamp;
+    const stashDir: string = path.join(workingDir, 'runs', runTimestamp);
+    fs.renameSync(currentRunDir, stashDir);
+    fs.copySync(ioDir, path.join(stashDir, 'io'));
+    fs.removeSync(ioDir);
+    fs.ensureDirSync(ioDir);
+    ensureRunsDir();
+  }
   res.status(200).send('Stashed');
 };
 
@@ -199,9 +236,9 @@ const handler = async (req: Request, res: Response) => {
     if (route === '/api/kits/$run') {
       await runKit(req, res);
     } else if (route === '/api/kits/$abort') {
-      await abortRun(req, res);
+      abortRun(res);
     } else if (route === '/api/kits/$stash') {
-      await stashRun(req, res);
+      stashRun(res);
     } else {
       res.status(404).send('Not found');
     }
@@ -225,6 +262,9 @@ const init = async () => {
     checkPackages();
     checkValidator();
     checkMaps();
+    ensureRunsDir();
+    abortRun();
+
     // TODO: This will not work in SEA mode
     // will need to have engine.js as asset and export to file.
     // + possibly another node.exe will be needed
