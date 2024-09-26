2357c5bdf2db260e013c2ebc3d9932914cc1d667
mechanik-daniel <daniel@outburn.co.il>
2024-09-26 03:44:15 +0300

    fix: prevent $validate process from throwing error

--- src/helpers/extraBindings/validate.ts
+++ src/helpers/extraBindings/validate.ts
@@ -14,9 +14,13 @@ export const validate = (resourceFileName: string, options?: Record<string, stri
     const igArg: string = options?.ig ? `-ig "${options.ig}"` : '';
     const outputArg: string = options?.output ? `-output "${pathJoin(ioFolder, options.output)}"` : '';
     const profileArg: string = options?.profile ? `-profile ${options.profile}` : '';
-    const command: string = `"${java}" -Dfile.encoding=UTF-8 -jar "${jar}" "${filePath}" -version 4.0.1 -jurisdiction global ${igArg} ${outputArg} ${profileArg} -tx n/a`;
-    const stdout: string = execSync(command, { stdio: ['ignore', 'pipe', 'ignore'], encoding: 'utf8' });
-    return stdout;
+    const command: string = `"${java}" -Dfile.encoding=UTF-8 -jar "${jar}" "${filePath}" -version 4.0.1 -jurisdiction global ${igArg} ${outputArg} ${profileArg} -tx n/a -extension http://hl7api.sourceforge.net/`;
+    try {
+      const stdout: string = execSync(command, { stdio: ['ignore', 'pipe', 'ignore'], encoding: 'utf8' });
+      return stdout;
+    } catch (e) {
+      return e instanceof Error ? e.message : JSON.stringify(e);
+    }
   } else {
     throw new Error('Failed to find JRE :(');
   }
