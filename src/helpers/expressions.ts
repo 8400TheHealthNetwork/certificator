fc14b92ac78087fd74b4fd84dacf77b1950e98fc
mechanik-daniel <daniel@outburn.co.il>
2024-09-29 13:27:10 +0300

    feat: add mock kit and fix kit running bugs

--- src/helpers/expressions.ts
+++ src/helpers/expressions.ts
@@ -111,3 +111,133 @@ export const validateTree: Expression = jsonata(`
     )
   `
 );
+
+export const addMockKit: Expression = jsonata(`(
+  $mockActions := [
+    {
+      "id": "wait1s",
+      "description": "Wait 1 second and then success",
+      "mapping": "mockAction1s"
+    },
+    {
+      "id": "wait1f",
+      "description": "Wait 1 second and then fail",
+      "mapping": "mockAction1f"
+    },
+    {
+      "id": "wait1e",
+      "description": "Wait 1 second and then error",
+      "mapping": "mockAction1e"
+    },
+    {
+      "id": "wait10s",
+      "description": "Wait 10 seconds and then success",
+      "mapping": "mockAction10s"
+    },
+    {
+      "id": "wait10f",
+      "description": "Wait 10 seconds and then fail",
+      "mapping": "mockAction10f"
+    },
+    {
+      "id": "wait10e",
+      "description": "Wait 10 seconds and then error",
+      "mapping": "mockAction10e"
+    }
+  ];
+  $allActions := $append($kits.actions, $mockActions);
+  $mockKit := {
+    "id": "mock-kit",
+    "name": "Mock Kit",
+    "description": "This kit contains mock tests and actions. It's purpose is to test behaviour of the orchestrator and UI",
+    "children": [
+      {
+        "id": "should-pass",
+        "name": "Tests That Pass",
+        "children": [
+          {
+            "id": "passing-tests1",
+            "name": "Passing Tests 1",
+            "children": [
+              {
+                "id": "passing-test1",
+                "name": "passing test 1",
+                "actions": ["wait1s"]
+              },
+              {
+                "id": "passing-test2",
+                "name": "passing test 2",
+                "actions": ["wait10s"]
+              }
+            ]
+          },
+          {
+            "id": "passing-tests2",
+            "name": "Passing Tests 2",
+            "children": [
+              {
+                "id": "passing-test3",
+                "name": "passing test 3",
+                "actions": ["wait10f", "wait10s"]
+              },
+              {
+                "id": "passing-test4",
+                "name": "passing test 4",
+                "actions": ["wait1e", "wait10s"]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "id": "should-fail",
+        "name": "Tests That Fail",
+        "children": [
+          {
+            "id": "failing-tests1",
+            "name": "failing Tests 1",
+            "children": [
+              {
+                "id": "failing-test1",
+                "name": "failing test 1",
+                "actions": ["wait10f", "wait1f"]
+              },
+              {
+                "id": "failing-test2",
+                "name": "failing test 2",
+                "actions": ["wait1f", "wait10s", "wait10f"]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "id": "should-error",
+        "name": "Tests That Throw Errors",
+        "children": [
+          {
+            "id": "error-tests1",
+            "name": "error Tests 1",
+            "children": [
+              {
+                "id": "error-test1",
+                "name": "error test 1",
+                "actions": ["wait10f", "wait1e"]
+              },
+              {
+                "id": "error-test2",
+                "name": "error test 2",
+                "actions": ["wait1e", "wait10f", "wait10e"]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  $allKits := $append($kits.kits, $mockKit);
+  {
+    'actions': $allActions,
+    'kits': $allKits
+  }
+)`);
