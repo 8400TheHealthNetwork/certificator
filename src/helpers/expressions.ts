e4f9d03190ecb444ff046b7832f63051e1c2dbe8
ILIA <ILIA@OUTBURN.CO.IL>
2025-03-30 14:53:07 +0300

    fix(Test 174)

--- src/helpers/expressions.ts
+++ src/helpers/expressions.ts
@@ -442,7 +442,7 @@ export const reportRunSettings: Expression = jsonata(`
     $recordedDateChart := $exists($conditionRecordedDateDistribution) ?
     {
       'id': 'recorded-date-chart',
-      'title': 'Recorded date distribution',
+      'title': 'Condition.recordedDate distribution (Test 174)',
       "type": "line",
       "data": [$conditionRecordedDateDistribution.{'label':date ,'value':count}]
     };
